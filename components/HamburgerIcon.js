9082875584f9ae1d19d474d037d4fcc292c5e022
ibrar1988 <inform2ibrar@gmail.com>
2019-05-14 14:56:15 +0530

    Initial commit

---
+++ components/HamburgerIcon.js
@@ -0,0 +1,30 @@
+class HamburgerIcon extends Component {
+
+    toggleDrawer = () => {
+  
+      this.props.navigationProps.toggleDrawer();
+  
+    }
+  
+    render() {
+  
+      return (
+  
+        <View style={{ flexDirection: 'row' }}>
+  
+          <TouchableOpacity onPress={this.toggleDrawer.bind(this)} >
+  
+            <Image
+              source={{ uri: 'https://reactnativecode.com/wp-content/uploads/2018/04/hamburger_icon.png' }}
+              style={{ width: 25, height: 25, marginLeft: 5 }}
+            />
+  
+          </TouchableOpacity>
+  
+        </View>
+  
+      );
+  
+  
+    }
+  }
